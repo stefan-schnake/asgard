692d8dd6b46f20e1284970991a397aa27594c570
Miroslav Stoyanov <30537612+mkstoyanov@users.noreply.github.com>
2024-08-06 15:38:48 -0400

    Input files (#717)
    * added input file capabilities

--- testing/sandbox.cpp
+++ testing/sandbox.cpp
@@ -4,8 +4,10 @@ using namespace asgard;
 
 using prec = asgard::default_precision;
 
-int main(int, char **)
+int main(int argc, char **argv)
 {
+  ignore(argc);
+  ignore(argv);
   // keep this file clean for each PR
   // allows someone to easily come here, dump code and start playing
   // this is good for prototyping and quick-testing features/behavior
